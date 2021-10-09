e965fa71bef6e6ff8c238a4e6258bf2dc674c86d
bforblack <39429703+bforblack@users.noreply.github.com>
2021-10-09 14:40:50 +0530

    Update BertModel.py
    Changes done fro extracting features

--- Bert/BertModel.py
+++ Bert/BertModel.py
@@ -37,15 +37,41 @@ class Sentiment:
         return InputExample(guid=None,text_a=data['Reviews'],text_b=None,label=data['Sentiments'])
 
 
-    def createFeature(self,data):
-        return None
+    def getTokenizedData(self,data,max_length=128):
+        input_dic=tokenizer._encode_plus(data,add_special_tokens=True,max_length=max_length,return_token_type_ids=True,
+                                         return_attention_mask=True,truncation_strategy=True,padding_strategy=True)
+
+        input_ids,attention_mask,token_type_ids=(input_dic['input_ids'],input_dic['attention_mask'],input_dic['token_type_ids'])
+
+        return InputFeatures(input_ids=input_ids,attention_mask=attention_mask,token_type_ids=token_type_ids)
 
 
 
 
+    def generateTensorData(self,features):
+        def gen():
+            for f in features:
+                yield (
+                    {
+                        "input_ids": f.input_ids,
+                        "attention_mask": f.attention_mask,
+                        "token_type_ids": f.token_type_ids,
+                    },
+                    f.label,
+                )
 
-
-
+        return  tf.data.Dataset.from_generator(
+        gen,
+        ({"input_ids": tf.int32, "attention_mask": tf.int32, "token_type_ids": tf.int32}, tf.int64),
+        (
+            {
+                "input_ids": tf.TensorShape([None]),
+                "attention_mask": tf.TensorShape([None]),
+                "token_type_ids": tf.TensorShape([None]),
+            },
+            tf.TensorShape([]),
+        ),
+    )
 
 
 
